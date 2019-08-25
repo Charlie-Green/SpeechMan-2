338ad192238d1814d15b7df52940e1b14c24b640
Charlie-Green <vadim.churun@gmail.com>
2019-08-25 08:07:29 +0300

    Initial commit

---
+++ app/src/main/java/by/vadim_churun/ordered/speechman2/SpeechManActivity.kt
@@ -0,0 +1,14 @@
+package by.vadim_churun.ordered.speechman2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class SpeechManActivity: AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        super.setContentView(R.layout.speechman_activity)
+    }
+}
