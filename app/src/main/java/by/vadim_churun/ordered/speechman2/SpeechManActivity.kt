7d284be65f0c5106748486bb33cd536ff18d996b
Charlie-Green <vadim.churun@gmail.com>
2019-08-25 09:40:24 +0300

    Implement navigation drawer

--- app/src/main/java/by/vadim_churun/ordered/speechman2/SpeechManActivity.kt
+++ app/src/main/java/by/vadim_churun/ordered/speechman2/SpeechManActivity.kt
@@ -2,13 +2,38 @@ package by.vadim_churun.ordered.speechman2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.speechman_activity.*
 
 
 class SpeechManActivity: AppCompatActivity()
 {
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // MANAGING NAVIGATION:
+
+    private fun setupNavigation()
+    {
+        navDrawer.setCheckedItem(R.id.miPeople)
+        navDrawer.setNavigationItemSelectedListener { selectedItem ->
+            if(navDrawer.checkedItem == selectedItem)
+                return@setNavigationItemSelectedListener true
+
+            // TODO: Navigate
+
+            drawerLayout.closeDrawers()
+            true
+        }
+
+        // TODO: React on destination change.
+    }
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // LIFECYCLE:
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         super.setContentView(R.layout.speechman_activity)
+        setupNavigation()
     }
 }
